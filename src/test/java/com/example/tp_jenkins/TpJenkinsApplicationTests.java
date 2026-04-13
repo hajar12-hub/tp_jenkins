4c889b77adcc3be4287c35a7841e96406dd3f3d2
hajar12-hub <azaouhajar@gmail.com>
2026-04-14 00:05:05 +0200

    update

---
+++ src/test/java/com/example/tp_jenkins/TpJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
